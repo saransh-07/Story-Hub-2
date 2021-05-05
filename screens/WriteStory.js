eafb8fa80255706d45e1dd3fe05f529de0046a94
saransh-07 <69812136+saransh-07@users.noreply.github.com>
2021-05-05 19:21:21 +0530

    Add files via upload

---
+++ screens/WriteStory.js
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import {View, Text, StyleSheet, TextInput , TouchableOpacity } from 'react-native';
+import db from '../config.js';
+import firebase from 'firebase';
+export default class Write extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            author : '',
+            title : '',
+            story : '',
+        }
+    }
+    submitStory=async()=>{
+db.collection('Stories').doc('Story0001').add({
+    'Author' : this.state.author,
+    'Title' : this.state.title,
+    'Story' : this.state.story
+})
+    }
+    render(){
+        return(
+            <View style = {styles.container}>
+            <View style = {styles.headingContainer}>
+                <Text style = {styles.heading}>
+                    Write Story
+                </Text>
+            </View>
+            <View>
+<TextInput style = {styles.inputBox} onChangeText = {text=>{this.setState({title : text})}} placeholder = '  Enter Story Title'/>
+<TextInput style = {styles.inputBox} onChangeText = {text=>{this.setState({author : text})}}placeholder = '  Enter Author Name(Yours infact)'/>
+<TextInput style = {[styles.inputBox,{height : 250}]} onChangeText={text=>{this.setState({story : text})}} placeholder = '  Your Story Here!' multiline = {true} />
+            </View>
+                 <TouchableOpacity style = {styles.button} onPress = {()=>{this.submitStory()}} >
+                     <Text style ={[styles.heading,{fontSize : 20}]}>
+                         Submit 
+                     </Text>
+                 </TouchableOpacity>
+            </View>
+          
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        display : 'flex',
+        flex : 1,
+        backgroundColor : 'green',
+        marginTop : 60
+    },
+    heading : {
+        fontWeight : 'bold',
+        fontSize : 40,
+        color : 'white',
+        textAlign : 'center'
+    },
+    headingContainer : {
+        marginTop : 20,
+        backgroundColor : '#00BFFF',
+        height : 150,
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center'
+
+    },
+    inputBox : {
+        width : '80%',
+        height : 60,
+        borderStyle : 'solid',
+       alignSelf :'center',
+        borderWidth : 3,
+        marginTop : 50,
+        backgroundColor : '#FAEBD7',
+       
+    },
+    button : {
+       
+        display : 'flex',
+        backgroundColor : '#00BFFF',
+        width : '30%',
+        height : 40,
+        justifyContent : 'center',
+        marginTop : 10,
+        alignSelf : 'center'
+    }
+})
